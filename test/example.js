eb5d5786c17e7b675e9ebf14fcc969a4702628bf
southbite <simon@tenacious.digital>
2018-02-06 16:15:52 +0200

    feature: tested triple wildcard

--- test/example.js
+++ test/example.js
@@ -7,7 +7,7 @@ describe.only('connections', function () {
 
   var CLIENTS_COUNT = 10;
 
-  var DATA_POINTS_COUNT = 100;
+  var DATA_POINTS_COUNT = 1000;
 
   var SUBS_PER_CLIENT_COUNT = 10;
 
@@ -23,7 +23,14 @@ describe.only('connections', function () {
 
   const dataPoints = random.randomPaths({count: DATA_POINTS_COUNT});
 
+  var segmentPaths3Count = 0;
+
+  dataPoints.forEach(function(point){
+    if (point.split('/').length == 3) segmentPaths3Count++;
+  });
+
   var RECEIVED = {};
+  var WILD_RECEIVED = {};
 
   it('sets up the cluster', function (done) {
 
@@ -69,19 +76,16 @@ describe.only('connections', function () {
     done();
   });
 
-  xit('sets up wildcard subscriptions', function (done) {
+  it('sets up wildcard subscriptions', function (done) {
 
     clients.forEach(function(client){
 
-      for (var i = 0; i < DATA_POINTS_COUNT; i++) {
+      client.subscribe('*/*/*', function(data){
 
-        client.subscribe(dataPoints[i], function(data){
+        if (WILD_RECEIVED[this.address] == null) WILD_RECEIVED[this.address] = 0;
+        WILD_RECEIVED[this.address]++;
 
-          if (RECEIVED[this.address] == null) RECEIVED[this.address] = 0;
-          RECEIVED[this.address]++;
-
-        }.bind(client));
-      }
+      }.bind(client));
     });
 
     done();
@@ -96,8 +100,6 @@ describe.only('connections', function () {
       var randomIndex = random.integer(0, CLIENTS_COUNT - 1);
 
       var randomClient = clients[randomIndex];
-
-      console.log('client publish:::', dataPoints[i]);
 
       randomClient.publish(dataPoints[i], {i:i, id:randomClient.address, path:dataPoints[i]});
     }
@@ -115,14 +117,28 @@ describe.only('connections', function () {
 
     clients.forEach(function(client){
       console.log('client.RECEIVED:::', client.address, RECEIVED[client.address]);
-      if (RECEIVED[client.address] != Object.keys(client.subscriptions).length) allReceived = false;
+      if (RECEIVED[client.address] != DATA_POINTS_COUNT) {
+        console.log('allReceived = false');
+        allReceived = false;
+      }
     });
 
-    if (!allReceived) return done('expected message');
+    console.log('client.WILD_RECEIVED should be:::', segmentPaths3Count);
+    console.log('-------------------------------------------------------');
+
+    clients.forEach(function(client){
+      console.log('client.WILD_RECEIVED:::', client.address, WILD_RECEIVED[client.address], segmentPaths3Count);
+      if (WILD_RECEIVED[client.address] != segmentPaths3Count) {
+        console.log('allReceived = false');
+        allReceived = false;
+      }
+    });
+
+    if (!allReceived) return done(new Error('expected received message counts to be accurate'));
 
     var subsDistributed = true;
 
-    var totalSubscriptions = clients.length * SUBS_PER_CLIENT_COUNT;
+    var totalSubscriptions = DATA_POINTS_COUNT + clients.length;
 
     console.log('-------------------------------------------------------');
     console.log('CLUSTER PEER SUBS COUNT SHOULD BE > 0 and < ' + totalSubscriptions / peers.length * 2);
